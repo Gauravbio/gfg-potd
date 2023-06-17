2806e71035f987d5d3b7d72b6a7673f9f3f65c3e
unknown <gp206524@gmail.com>
2023-06-17 19:30:09 +0530

    17-JUNE-23

---
+++ 17-JUNE-23.cpp
@@ -0,0 +1,16 @@
+class Solution{
+    public:
+    // Function to insert element into the queue
+    unordered_map<int,int> mp;
+    void insert(queue<int> &q, int k){
+        mp[k]++;
+        q.push(k);
+    }
+    
+    // Function to find frequency of an element
+    // return the frequency of k
+    int findFrequency(queue<int> &q, int k){
+        return mp[k];
+    }
+    
+};
