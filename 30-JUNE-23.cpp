3004507ec814772659e4d937de11a30ceae3345a
unknown <gp206524@gmail.com>
2023-06-30 17:27:18 +0530

    23-JUNE

--- 30-JUNE-23.cpp
+++ 30-JUNE-23.cpp
@@ -21,22 +21,3 @@ public:
 	}
 
 };
-
-//Java Code
-class Solution {
-    int isDivisible(String s) {
-        long rem=0;
-        int n=s.length();
-        
-        for(int i=0;i<n;i++) {
-            if(s.charAt(i)=='1') {
-                if(i%2==1) rem+=2;
-                else rem+=1;
-            }
-        }
-        
-        rem%=3;
-        if(rem!=0) return 0;
-        return 1;
-    }
-}
